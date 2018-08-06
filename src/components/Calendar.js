1808ca7a311ae0686a2c08dce309c2d76fd2b5cf
Soheil <Soheil.Tajahmadi@gmail.com>
2018-08-06 16:39:05 +1000

    adding some empty components

---
+++ src/components/Calendar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Calendar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+
+export default Calendar;
