e80aa7254b7ce69fd0cf3a53e2a213f91d9a59fd
williamlieu <william.lieu@outlook.fr>
2018-08-07 19:09:08 +1000

    firstmerge

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-
-
-
-
+import User from './components/User'
+import axios from 'axios'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>New Group</h1>
-
+      <Group/>
+      <User/>
 
       </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import Groups from './Groups.js'
 
-
-
-
 class Home extends Component {
   render() {
     return (
