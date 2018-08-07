f9f13c1d7ce761200ae788831a8f5a5f081406ec
Soheil <Soheil.Tajahmadi@gmail.com>
2018-08-08 09:45:16 +1000

    resolving conflicts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import Groups from './Groups.js'
-=======
-import React, { Component } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Interests from './Interests.js'
->>>>>>> 8c15025f2d94a519e3c31f4310f5239122efa2d5
 
 class Home extends Component {
   render () {
