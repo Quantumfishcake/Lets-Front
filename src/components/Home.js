68478419c9c1c2e6abead9a0fd1ffb4ad2c62846
williamlieu <william.lieu@outlook.fr>
2018-08-08 16:30:02 +1000

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
+
 import React, { Component } from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import Interests from './Interests.js'
-import Loginpic from './Loginpic'
-=======
-
-import React, { Component } from 'react';
-import Groups from './Groups'
-import Header from './Header'
-import Footer from './Footer'
 import Interests from './Interests'
+import Loginpic from './Loginpic'
+import Groups from './Groups'
 
->>>>>>> 63938ffb75d4c4d84ff14706c69134204a001360
 
 class Home extends Component {
   render () {
