05bb7d624065689461616b0a697c235d8fc4027c
Tom Quant <tom@Toms-MacBook-Air.local>
2018-08-08 19:47:45 +1000

    css merges

--- src/components/Events.js
+++ src/components/Events.js
@@ -20,64 +20,50 @@ class Events extends Component {
       })
     })
   }
-  render () {
+  render() {
     const { location } = this.props
     const { events } = this.state
     const date = url.parse(location.search, true).query.filterBy
     const eventsFiltered = location.search == '' ? events : _.filter(events, { date })
-   
+
     console.log(location)
 
     return (
-      <div>
-        <Header />
-        <h1>Events Page here</h1>
-        <Calendar2 date={date} />
-        <div>
-          <div className="create">
-          <Link to={
-            {
-              pathname: '/newevent/',
+      <div className='maincontainer'>
+        <div className='container'>
+          <div className='col-sm-12'>
+            <Header />
+            <div className='col-sm-4 sidebar'>
+              <Calendar2 date={date} />
+              <div className='col-sm-8 page-content'>
+                <Link to={
+                  {
+                    pathname: '/newevent/',
+                    state: { from: this.props.location }
+                  }
+                }>Create a new Event</Link>
 
-              state: {from: this.props.location}
-          }
-        }>Create a new Event</Link> </div>
-          {this.state.events.map( event => {
-          return (
+                {eventsFiltered.map(event => {
 
-          <div className="container">
-
-            <h3 className="eventtitle">
-              <Link to={{pathname: '/events/' + event.id}} >{event.name}</Link>
-            </h3>
-                <p>{event.description}</p>
-
-          </div>
-        )
-        })  }
-
-              state: { from: this.props.location }
-            }
-          }>Create a new Event</Link>
-          
-          {eventsFiltered.map(event => {
-            
-            return (
-              <div>
-                <h3>
-                  {event.name}
-                </h3>
-                <h4>Where : {event.location}</h4>
-                <p>Description : {event.description}</p>
-                <p>event_id : {event.id}</p>
-                <p>date: {event.date}</p>
-                <p>More details : <Link to={{ pathname: '/events/' + event.id }} >{event.name}</Link></p>
+                  return (
+                    <div className='eventtitle'>
+                      <h3>
+                        {event.name}
+                      </h3>
+                      <h4>Where : {event.location}</h4>
+                      <p>Description : {event.description}</p>
+                      <p>event_id : {event.id}</p>
+                      <p>date: {event.date}</p>
+                      <p>More details : <Link to={{ pathname: '/events/' + event.id }} >{event.name}</Link></p>
+                    </div>
+                  )
+                })}
               </div>
-            )
-          })}
-
+            </div>
+          </div>
         </div>
 
+      </div>
     )
   }
 }
