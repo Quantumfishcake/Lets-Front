f05b7c3d8997d98fb9d9d3bc00aee3e911b8044d
Soheil <Soheil.Tajahmadi@gmail.com>
2018-08-08 23:18:57 +1000

    added user login functionality, initial stage

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,37 +2,46 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Header from './Header'
 
-const LOGIN_SERVER_URL='http://localhost:3000/users/'
+const LOGIN_SERVER_URL='http://localhost:3000/'
 
 class LogIn extends Component {
   constructor() {
     super()
     this.state={
 
-      user_info: {
+      auth: {
         email: '',
         password: ''}
      }
      this._handleEmailInput=this._handleEmailInput.bind(this)
      this._handlePasswordInput=this._handlePasswordInput.bind(this)
+     this._handleSubmit=this._handleSubmit.bind(this)
 }
 
 
 _handleEmailInput(e) {
   this.setState({
-    user_info: { ...this.state.user_info, email: e.target.value }
+    auth: { ...this.state.auth, email: e.target.value }
   })
 
 }
 
 _handlePasswordInput(e) {
   this.setState({
-    user_info: { ...this.state.user_info, password: e.target.value}
+    auth: { ...this.state.auth, password: e.target.value}
   })
 }
 
 _handleSubmit(e) {
+  e.preventDefault();
 
+  axios.post(LOGIN_SERVER_URL + 'user_token', this.state ).then( (result) => {
+    console.log("Response came back:", result);
+    localStorage.setItem("jwt", result.data.jwt);
+    localStorage.setItem("username", this.state.auth.email)
+  }).catch( (errors) => {
+    console.log("Errors came back:",  errors);
+  })
 
  }
 
@@ -43,11 +52,11 @@ _handleSubmit(e) {
         <form onSubmit={this._handleSubmit}>
           <label>
             Email:
-            <input onChange={this._handleEmailInput} type="text" name="email" value={this.state.user_info.email} autoFocus></input>
+            <input onChange={this._handleEmailInput} type="text" name="email" value={this.state.auth.email} autoFocus></input>
           </label>
           <label>
             Password:
-            <input onChange={this._handlePasswordInput} type="password" name="password" value={this.state.user_info.password} ></input>
+            <input onChange={this._handlePasswordInput} type="password" name="password" value={this.state.auth.password} ></input>
           </label>
           <button type="submit">Log in</button>
         </form>
